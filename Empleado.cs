01bfcb2c99cb3a8a5930dca9781eed99f6fa5046
AngelMatiasA <angel.apaza010@gmail.com>
2023-07-02 02:12:57 -0300

    usando enum

---
+++ Empleado.cs
@@ -0,0 +1,30 @@
+namespace EspacioEmpresa;
+/* Auxiliar;
+Administrativo; Ingeniero; Especialista; Investigador.*/
+public enum {
+    Auxiliar;
+
+}
+
+public class Empleado
+{
+   /* 
+    1. Crear una clase para almacenar la siguiente información: una clase Empleado con
+los siguientes atributos:
+a. Nombre (string), Apellido (string), Fecha de nacimiento (datetime),
+Estado civil (char), Género (char), fecha de ingreso en la empresa
+(datetime), sueldo Básico (double), Cargo (cargos)
+b. La enumeración cargos con los siguientes elementos: Auxiliar;
+Administrativo; Ingeniero; Especialista; Investigador. (investigue enum
+en c# para realizar esto)*/
+ private string Nombre = " ";
+ private string apellido = " ";
+ private DateTime fechaIngreso;
+ private double sueldoBasico;
+ 
+ private char estadoCivil;
+
+ 
+
+
+}
