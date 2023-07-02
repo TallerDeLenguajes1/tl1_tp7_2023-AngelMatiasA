ab3d344bcca40caff9e780ffb0af4471aece4075
AngelMatiasA <angel.apaza010@gmail.com>
2023-07-02 02:47:49 -0300

    empresa 2a

--- Empleado.cs
+++ Empleado.cs
@@ -1,30 +1,75 @@
 namespace EspacioEmpresa;
 /* Auxiliar;
-Administrativo; Ingeniero; Especialista; Investigador.*/
-public enum {
-    Auxiliar;
-
+; Ingeniero; Especialista; Investigador.*/
+public enum Cargo {
+    Auxiliar,
+    Administrativo,
+    Ingeniero,
+    Especialista,
+    Investigador
 }
 
 public class Empleado
 {
-   /* 
-    1. Crear una clase para almacenar la siguiente información: una clase Empleado con
-los siguientes atributos:
-a. Nombre (string), Apellido (string), Fecha de nacimiento (datetime),
-Estado civil (char), Género (char), fecha de ingreso en la empresa
-(datetime), sueldo Básico (double), Cargo (cargos)
-b. La enumeración cargos con los siguientes elementos: Auxiliar;
-Administrativo; Ingeniero; Especialista; Investigador. (investigue enum
-en c# para realizar esto)*/
+   
  private string Nombre = " ";
  private string apellido = " ";
+ private DateTime fechaNac;
+  private char estadoCivil;
+   private char genero;
  private DateTime fechaIngreso;
  private double sueldoBasico;
- 
- private char estadoCivil;
+ private Cargo cargo;
 
- 
+    public string Nombre1 { get => Nombre; set => Nombre = value; }
+    public string Apellido { get => apellido; set => apellido = value; }
+    public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public char Genero { get => genero; set => genero = value; }
+    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+    public Cargo Cargo { get => cargo; set => cargo = value; } 
 
+//      Calcular lo siguiente:
+// i. La antigüedad del empleado en la empresa.
+// ii. La edad del empleado,
+// iii. La cantidad de años que le falta al empleado para poder
+// jubilarse (para la mujer la edad es 60 años, para el varón es 65
+// años).
+
+public int antiguedad( DateTime fechaIngreso){
+    DateTime hoy = DateTime.Now;
+    int anios = 0; 
+    anios = fechaIngreso.Year - hoy.Year; 
+    if( anios != 0){
+        if (fechaIngreso.Month >= hoy.Month && fechaIngreso.Day >= hoy.Day  )
+        {
+               
+            return anios;
+            
+        }else
+        {
+            anios = anios -1; 
+        }
+
+    }  
+        return anios;
+   
+}
+
+public int aniosParaJub( int edad, char genero){
+   int aniosJub = 0; 
+    if (genero == 'm')
+    {
+        aniosJub = 60 - edad;
+        
+    } else if (genero == 'f' )
+    {
+         aniosJub = 65 - edad;
+        
+    }
+
+    return aniosJub;
+}
 
 }
